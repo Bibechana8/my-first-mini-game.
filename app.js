1242334831fd30535b3ee017392f91005b289b4d
Bibechana8 <bibechanapoudel25@gmail.com>
2026-02-14 19:29:15 +0545

    some more logic

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 let boxes = document.querySelectorAll(".box");
-let resetBtn = document.querySelectorAll("#reset-btn");
+let resetBtn = document.querySelector("#reset-btn");
+let newGameBtn = document.querySelector("#new-btn");
+let msgContainer = document.querySelector(".msg-container");
+let msg = document.querySelector("#msg");
 
 let turn0 = true;
 
@@ -25,5 +28,26 @@ boxes.forEach((box) => {
 
         }
         box.disabled = true;
+        checkWinner();
     });
 });
+const showWinner = (Winner) => {
+    msg.innerText =`Congratulations,Winner is ${Winner}`;
+    msgContainer.classList.remove("hide");
+}
+
+
+
+const checkWinner = () => {
+    for (let pattern of winPatterns){
+        let pos1Val = boxes[pattern[0]].innerText;
+        let pos2Val = boxes[pattern[1]].innerText;
+        let pos3Val = boxes[pattern[2]].innerText;
+        if(pos1Val !="" && pos2Val !="" && pos3Val !=""){
+            if(pos1Val === pos2Val && pos2Val === pos3Val){
+                console.log("Winner ",pos1Val);
+                showWinner(pos1Val);
+        }
+    }
+}
+};
