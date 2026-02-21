1b4d0df91026d5965180a825cb8fef984bdd1b80
Bibechana8 <bibechanapoudel25@gmail.com>
2026-02-21 21:49:42 +0545

    changes

--- second.js
+++ second.js
@@ -1,8 +1,28 @@
-let gamenum=25;
-let usernum=prompt("Guess the game number:");
+//  practice of loops and string methods
+// let gamenum=25;
+// let usernum=prompt("Guess the game number:");
 
-while(usernum!=gamenum){
-    usernum=prompt("You entered the wrong number,guess again:");
+// const { use } = require("react");
 
-}
-console.log("Congratulations! You guessed the correct number!");
+// while(usernum!=gamenum){
+//     usernum=prompt("You entered the wrong number,guess again:");
+
+// }
+// console.log("Congratulations! You guessed the correct number!");
+
+// let str="   Apna college    ";
+// console.log(str.trim());
+
+
+// let str="Apna college";
+// str=str.replace("A","S");
+// console.log(str);
+
+let user= prompt("Enter your full name:");
+let result="@" + user + user.length;
+console.log(result);
+// let string1= "Shradhakhapra";
+// let string2="@";
+// let string3=string1.length
+// let result=string2.concat(string1)+string3;
+// console.log(result);
