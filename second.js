af0dd5523151b1b99fdffb85d270e8af697cbf4a
Bibechana8 <bibechanapoudel25@gmail.com>
2026-02-21 16:56:46 +0545

    change

---
+++ second.js
@@ -0,0 +1,8 @@
+let gamenum=25;
+let usernum=prompt("Guess the game number:");
+
+while(usernum!=gamenum){
+    usernum=prompt("You entered the wrong number,guess again:");
+
+}
+console.log("Congratulations! You guessed the correct number!");
